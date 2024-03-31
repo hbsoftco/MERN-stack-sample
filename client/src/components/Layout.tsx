536c1cec359571e937816682d866d0a14bc1103d
hossein <hossein.bajan@gmail.com>
2024-04-01 00:33:52 +0330

    Add navigation in header

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,16 +1,22 @@
 import { ReactNode } from "react";
+import { useNavigate } from "react-router-dom";
 
 interface LayoutProps {
   children: ReactNode;
 }
 
 const Layout = ({ children }: LayoutProps) => {
+  const navigate = useNavigate();
+
   return (
     <div className="m-auto container">
       <div className="bg-gray-800 text-white p-5 ">
-        <h1 className="text-2xl font-bold cursor-pointer">
+        <button
+          className="text-2xl font-bold cursor-pointer bg-transparent border-none"
+          onClick={() => navigate("/")}
+        >
           hbsoft blog - MERN
-        </h1>
+        </button>
       </div>
       <div className="p-5">{children}</div>
     </div>
